be606cc74d4b56c86d49252417e4dbf4142af386
Yuri Fernandes <yuri_fernandes2000@hotmail.com>
2025-10-11 00:13:58 -0300

    Atividade teste unitário

---
+++ 0x02/Person.java
@@ -0,0 +1,34 @@
+
+public class Person {
+    private String usuario;
+    private String password;
+
+    public Person() {
+        this.usuario = "";
+        this.password = "";
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public boolean checkUser() {
+        return this.usuario.length() >= 8 && !this.usuario.matches("^[a-zA-Z0-9@#$%&*]+$");
+    }
+
+    public boolean checkPassword() {
+        return this.password.length() >= 8 && this.password.matches(".*\\d.*") && this.password.matches(".*[A-Z].*") && this.password.matches("^[a-zA-Z0-9@#$%&*]+$");
+    }
+
+    public void setUsuario(String string) {
+        this.usuario = string;
+    }
+
+    public void setPassword(String string) {
+        this.password = string;
+    }
+}
---
+++ 0x02/PersonTest.java
@@ -0,0 +1,84 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import com.unit.test.demo.Person;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class PersonTest {
+    private Person person;
+
+    @BeforeAll
+    public void setup() {
+        person = new Person();
+    }
+
+    @Test
+    public void check_user_valid () {
+        person.setUsuario("PaulMcCartney2");
+        person.setPassword("NeilArms2");
+        Assertions.assertTrue(person.checkUser());
+        Assertions.assertFalse(person.checkPassword());
+    }
+
+    @Test
+    public void check_user_not_valid() {
+        person.setUsuario("Paul#McCartney");
+        person.setPassword("Neil@Arms");
+        Assertions.assertFalse(person.checkUser());
+        Assertions.assertFalse(person.checkPassword());
+    }
+
+    @Test
+    public void does_not_have_letters() {
+        person.setUsuario("123456789");
+        person.setPassword("#$%1234");
+
+        Assertions.assertFalse(person.checkUser());
+        Assertions.assertFalse(person.checkPassword());
+    }
+
+    @Test
+    public void does_not_have_numbers() {
+        person.setUsuario("Abcabcdefgh@");
+        person.setPassword("#hbtn@%tc");
+
+        Assertions.assertFalse(person.checkUser());
+        Assertions.assertFalse(person.checkPassword());
+    }
+
+    @Test
+    public void does_not_have_eight_chars() {
+        person.setUsuario("Abc@123");
+        person.setPassword("12$@hbt");
+
+        Assertions.assertFalse(person.checkUser());
+        Assertions.assertFalse(person.checkPassword());
+    }
+
+    @Test
+    public void check_password_valid() {
+        person.setUsuario("abC123456$");
+        person.setPassword("Hbtn@1234");
+
+        Assertions.assertFalse(person.checkUser());
+        Assertions.assertTrue(person.checkPassword());
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "abC123456$,Hbtn@1234",
+        "Betty@1#2,Hbtn@123"
+    })
+    public void check_password_valid_parametrized(String usuario, String password) {
+        person.setUsuario(usuario);
+        person.setPassword(password);
+
+        Assertions.assertFalse(person.checkUser());
+        Assertions.assertTrue(person.checkPassword());
+    }
+
+}
